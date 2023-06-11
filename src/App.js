d10392d4b3b91a9b7d137016e022261c22574a4a
Devaraj Rangannan <87937446+DevarajRangannan@users.noreply.github.com>
2023-06-11 05:28:01 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          ... Vanakkonda Maapla New Updatu ...
+          Hello World
         </p>
       </header>
     </div>
